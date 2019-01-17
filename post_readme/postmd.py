22e9b4f1e5549787cb941dad3d7faadf3b806d78
jlshix <jlshix@163.com>
2019-01-17 18:01:45 +0800

    refactoried with argparse rather than getopt

--- post_readme/postmd.py
+++ post_readme/postmd.py
@@ -1,38 +1,16 @@
-"""
-"""
-
 import os
 import sys
 import logging
 import shlex
 import subprocess
 from textwrap import dedent
-from getopt import getopt
+import argparse
 from datetime import datetime
 
 import requests
 
 domain = 'http://leoshi.me'
 server = 'http://jlshix.com:4000'
-
-def usage():
-    '''print the usage of this script'''
-    usage = '''
-    generate hexo article
-    you should exec this script at //source/_posts or its subdirectory
-
-    Usage:
-    python3 postmd.py -t TAGS -c CATEGORIES [-d DATETIME] file_path
-    e.g. python3 postmd.py -t t1,t2,t3 -c c1,c2 ../dmzj/README.md
-    
-    filepath    md file from local directory or github
-        if at github, url should starts with `https://raw.githubusercontent.com`
-    -t          add `tags` attribute to file header, separated by comma
-    -c          add `categories` attribute to file header, separated by comma
-    -d          specify `date` attribute in format like '2019-01-16,16:41:42'
-    '''
-    print(usage)
-
 
 def shell(command: str) -> str:
     """exec shell command and return output string"""
@@ -78,9 +56,11 @@ def make_header(title, date, tags, categories) -> str:
     return header
 
 
-def hexo_new(opts: list, uri: str) -> str:
-    '''generate hexo article by extracting params from opts and args parsed by getopt'''
+def hexo_new(args):
+    '''generate hexo article by extracting params from args'''
+    uri = args.uri
 
+    logging.info('featching file...')
     # get file content from github or local directory
     if uri.startswith('https://raw.githubusercontent.com'):
         resp = requests.get(uri)
@@ -97,39 +77,58 @@ def hexo_new(opts: list, uri: str) -> str:
     title = text[l+2:h].strip()
     text = text[h:]
 
+    logging.info('generating header...')
     # get params
-    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    for opt, value in opts:
-        if opt == '-t':
-            tags = value
-        elif opt == '-c':
-            categories = value
-        elif opt == '-d':
-            date = value.replace(',', ' ')
-    
+    tags, categories, date, name = args.t, args.c, args.d, args.n
+    if not date:
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    if not name:
+        name = title.replace(' ', '-')+'.md'
+    if not name.endswith('.md'):
+        name += '.md'
     # generate header
     header = make_header(title, date, tags, categories)
 
     # save file
-    with open(title.replace(' ', '-')+'.md', 'w') as f:
+    logging.info('saving file {}...'.format(name))
+    with open(name, 'w') as f:
         f.write(header)
         f.write(text)
+    logging.info('done generating new file')
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG, format=' %(levelname)s: %(message)s')
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
 
-    # get argv
-    opts, args = getopt(sys.argv[1:], 't:c:d:')
-    
-    # generate article
-    if not args:
-        logging.error('please specify a file uri')
-        usage()
-        exit(-2)
-    hexo_new(opts, args[0])
-    deploy()
-    
+    # root parser with description, all text below are aranged into `--help`
+    parser = argparse.ArgumentParser(description='generate new hexo file and deply for hexo, you should exec this script at //source/_posts or its subdirectory')
+    # specify subcommand
+    subparsers = parser.add_subparsers(dest='cmd')
 
-if __name__ == '__main__':
+    # subcommand `deploy` with option `-clean`, TODO `-preview`
+    deployer = subparsers.add_parser('deploy', description='deploy to github pages')
+    deployer.add_argument('-c', '-clean', action='store_true', help='clean before generate')
+
+    # subcommand `new`
+    gene = subparsers.add_parser('new', description='new hexo article from local or github')
+    gene.add_argument('uri', help=('md file from local directory or github'
+            'if at github, url should starts with `https://raw.githubusercontent.com`'))
+    gene.add_argument('-t', '-tag', help='add `tags` attribute to file header, separated by comma')
+    gene.add_argument('-c', '-cat', help='add `categories` attribute to file header, separated by comma')
+    gene.add_argument('-d', '-date', help='specify `date` attribute in format like `2019-01-16,16:41:42`')
+    gene.add_argument('-n', '-name', help='default name is title in content, specify to rename it')
+
+    args = parser.parse_args()
+    logging.info(args)
+
+    if not args.cmd:
+        logging.error('no command specified')
+        exit(-1)
+    elif args.cmd == 'deploy':
+        deploy(clean=args.c)
+    elif args.cmd == 'new':
+        hexo_new(args) 
+    # else option is not necessary
+
+if __name__ == "__main__":
     main()
